9d5ebec13f566a3c610becb88c6e5fc1d95799f7
poppen <poppen.jp@gmail.com>
2015-03-27 13:18:49 +0900

    fix typo

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ export CHEF_CACHE_DIR=${WERCKER_CACHE_DIR}/wercker/chefdk
 
 mkdir -p "$CHEF_CACHE_DIR"
 if [ ! -f "${CHEF_CACHE_DIR}/${CHEF_DEB}" ]; then
-    wget --no-check-certificate -P "$CHEF_CACHE_DIR" "https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/${86_64CHEF_DEB}"
+    wget --no-check-certificate -P "$CHEF_CACHE_DIR" "https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/${CHEF_DEB}"
 fi
 sudo dpkg -i "${CHEF_CACHE_DIR}/${CHEF_DEB}"
 
